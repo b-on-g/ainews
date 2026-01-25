5f63148d92958af2f18846e786784df5e09ba52b
Dev-cmyser <Dev-cmyser@users.noreply.github.com>
2026-01-25 07:14:02 +0000

    Deploying to gh-pages from @ b-on-g/ainews@4b31e653547b1f1a34cb74bea606cea3dddd8f37 🚀

--- web.mjs
+++ web.mjs
@@ -6212,7 +6212,7 @@ var $;
 "use strict";
 var $;
 (function ($) {
-    $mol_style_attach("mol/button/minor/minor.view.css", "[mol_button_minor]:not([disabled]) {\n\tcolor: var(--mol_theme_control);\n}\n");
+    $mol_style_attach("mol/button/minor/minor.view.css", "[mol_button_minor]:where(:not([disabled])) {\n\tcolor: var(--mol_theme_control);\n}\n");
 })($ || ($ = {}));
 
 ;
@@ -9059,7 +9059,7 @@ var $;
             trigger_content() {
                 return [
                     ...this.option_content(this.value()),
-                    this.Trigger_icon(),
+                    ...this.trigger_enabled() ? [this.Trigger_icon()] : [],
                 ];
             }
             menu_content() {
